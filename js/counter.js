5db17a9edbf40938a373cf522673a3db6040b23d
Katie Collins <88351482+plansyy@users.noreply.github.com>
2023-03-21 21:54:33 +0000

    Tue, Mar 21, 2023, 2:54 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -5,11 +5,15 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target')
-        const c = counter.innerText
-        const increment = 1
+        const c = +counter.innerText
+        const increment = target / 200
 
         if (c < target) {
-            counter.innerText = '${Math}'
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        }
+        else {
+            counter.innerText = target
         }
     }
 
