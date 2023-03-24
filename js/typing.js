0253a2d97d16d96c273029fa497449ef20d14950
Katie Collins <88351482+plansyy@users.noreply.github.com>
2023-03-24 22:12:25 +0000

    Fri, Mar 24, 2023, 3:12 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -2,10 +2,17 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 const text = "We love programming"
 let idx = 1
-let speed = 300
+let speed = 300 / speedEl.value
+speedEl.addEventListener('input', (element) => {
+    speed = 300 / element.target.value
+})
 
 writeText()
 
 function writeText(){
-    textEl.innerText
+    textEl.innerText = text.slice(0,idx)
+    idx++
+    if (idx > text.length) {
+        idx = 1
+    }
 }
