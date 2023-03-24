b3700b37e02f62dff44e921ef081a1f9149bfe28
Katie Collins <88351482+plansyy@users.noreply.github.com>
2023-03-24 21:57:25 +0000

    Fri, Mar 24, 2023, 2:57 PM -07:00

---
+++ js/typing.js
@@ -0,0 +1,11 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = "We love programming"
+let idx = 1
+let speed = 300
+
+writeText()
+
+function writeText(){
+    textEl.innerText
+}
